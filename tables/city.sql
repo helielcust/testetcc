0fc658e461b6991ec87c17e046021fd96333a520
Heliel <helieldamaceno@gmail.com>
2025-04-16 23:11:54 -0400

    Atualização automática

--- tables/city.sql
+++ tables/city.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `city` (
   `city_id` smallint unsigned NOT NULL AUTO_INCREMENT,
-  `city` varchar(165) NOT NULL,
+  `city` varchar(71) NOT NULL,
   `teste` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   `country_id` smallint unsigned NOT NULL,
